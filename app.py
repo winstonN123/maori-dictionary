0338bf09ff582bc27372a35474606858c8feb598
winston <18176@wc.school.nz>
2022-05-11 23:19:58 +1200

    lesson I "finished"

--- app.py
+++ app.py
@@ -22,11 +22,19 @@ def create_connection(db_file):
 
 
 def is_logged_in():
-    if session.get('email') is None:
+    if session.get('email') == None:
         print("not logged in ")
         return False
     else:
         print("logged in")
+        return True
+
+def is_admin():
+    if session.get('admin') != 1:
+        print("not admin ")
+        return False
+    else:
+        print("is admin")
         return True
 
 
@@ -63,7 +71,7 @@ def wordbank_list():
 
 @app.route('/')
 def main():
-    return render_template("home.html",categories=categories(),logged_in=is_logged_in(),wordbank_list=wordbank_list())
+    return render_template("home.html",categories=categories(),logged_in=is_logged_in(),wordbank_list=wordbank_list(),is_admin=is_admin())
 
 
 @app.route("/add_category",methods=['POST','GET'])
@@ -83,7 +91,7 @@ def admin():
 
         con.commit()
         con.close()
-    return render_template('add_category.html', logged_in=is_logged_in(), categories=categories())
+    return render_template('add_category.html', logged_in=is_logged_in(), categories=categories(),is_admin=is_admin())
 
 
 @app.route("/login",methods=['POST','GET'])
@@ -95,7 +103,7 @@ def login():
         print(request.form)
         email = request.form['email'].lower().strip()
         password=request.form['password'].strip()
-        query = """SElECT id,firstname,password from login WHERE email = ?"""
+        query = """SElECT id,firstname,password,admin FROM login WHERE email = ?"""
         con = create_connection(DATABASE)
         cur = con.cursor()
         cur.execute(query,(email,))
@@ -106,18 +114,19 @@ def login():
             user_id = user_data[0][0]
             firstname = user_data[0][1]
             db_password = user_data[0][2]
+            admin = user_data[0][3]
         except IndexError:
             return redirect("/login?error=Email+invalid+or+password+incorrect")
 
         if not bcrypt.check_password_hash(db_password,password):
             return redirect(request.referrer + '?error=Email+invalid+or+password+incorrect')
-
+        session['admin'] = admin
         session['email'] = email
         session['user_id'] = user_id
         session['firstname'] = firstname
         print(session)
         return redirect('/')
-    return render_template('login.html',logged_in=is_logged_in(),categories=categories())
+    return render_template('login.html',logged_in=is_logged_in(),categories=categories(),is_admin=is_admin())
 
 
 @app.route("/signup",methods=['POST','GET'])
@@ -153,7 +162,7 @@ def signup():
 
     if error == None:
         error=""
-    return render_template("signup.html",error=error,logged_in=is_logged_in(),categories=categories())
+    return render_template("signup.html",error=error,logged_in=is_logged_in(),categories=categories(),is_admin=is_admin())
 
 
 @app.route('/logout',methods=['POST','GET'])
@@ -164,28 +173,65 @@ def logout():
     return redirect(request.referrer + '?message=See+you+next+time!')
 
 
-@app.route('/category/<category_id>')
-def category_pages(category_id):
+@app.route('/category/<category_id>/<confirmation>')
+def category_pages(category_id,confirmation):
+    if confirmation == "yes":
+        query="DELETE FROM category WHERE id = ?"
+        con=create_connection(DATABASE)
+        cur=con.cursor()
+        cur.execute(query,(category_id,))
+        con.commit()
+        con.close()
+        return redirect("/")
+    if confirmation == "no":
+        return redirect("/category/{}/0".format(category_id))
+
     try:
         category_id = int(category_id)
     except ValueError:
         print("{} is not an integer".format(category_id))
         return redirect("/menu?error=Invalid+product+id")
-    return render_template("category.html",wordlist=wordbank_list(),categories=categories(),category_id=category_id,logged_in=is_logged_in())
+    return render_template("category.html",wordlist=wordbank_list(),categories=categories(),category_id=category_id,logged_in=is_logged_in(),is_admin=is_admin(),confirmation=confirmation)
 
 
-@app.route('/word/<word_id>/<cofirmation>')
-def word_page(word_id,confirmation):
-    try:
-        word_id = int(word_id)
-    except ValueError:
-        print("{} is not an integer".format(word_id))
-        return redirect("/menu?error=Invalid+product+id")
-    return render_template("word.html",wordlist=wordbank_list(),categories=categories(),word_id=word_id,logged_in=is_logged_in(),username=username(),)
+@app.route('/word/<word_id>/<confirmation>/<category_id>',methods=['POST','GET'])
+def word_page(word_id,confirmation,category_id):
+    word_id = int(word_id)
+    if confirmation == "yes":
+        query="DELETE FROM wordbank WHERE id = ?"
+        con=create_connection(DATABASE)
+        cur=con.cursor()
+        cur.execute(query,(word_id,))
+        con.commit()
+        con.close()
+        return redirect("/category/{}".format(category_id))
+    if confirmation == "no":
+        return redirect("/category/{}".format(category_id))
+
+    if request.method == 'POST':
+        print(request.form)
+        Modify_maori = request.form.get('Modify_maori').strip().title()
+        Modify_english = request.form.get('Modify_english').strip().title()
+        Modify_level = request.form.get('Modify_level')
+        Modify_definition = request.form.get('Modify_definition')
+        Date = datetime.now()
+        user_id = session.get('user_id')
+        print(user_id)
+        con=create_connection(DATABASE)
+        try:
+            query = "UPDATE wordbank SET maori = ? ,english = ?  ,definition = ?  ,level = ? ,last_modify_by = ?  ,editted_date = ?   WHERE id = ?"
+        except sqlite3.IntegrityError:
+            redirect('/signup?error=Passwords+dont+match')
+        cur = con.cursor()
+        cur.execute(query,(Modify_maori,Modify_english,Modify_definition,Modify_level,user_id,Date,word_id))
+        con.commit()
+        con.close()
+
+    return render_template("word.html",wordlist=wordbank_list(),categories=categories(),word_id=word_id,logged_in=is_logged_in(),username=username(),confirmation=confirmation,category_id=category_id,is_admin=is_admin())
 
 
-@app.route("/add_words/<choosen_category_id>",methods=['POST','GET'])
-def add_words(choosen_category_id):
+@app.route("/add_words",methods=['POST','GET'])
+def add_words():
     if request.method == 'POST':
         print(request.form)
         Maori_word = request.form.get('Maori').strip().title()
@@ -193,7 +239,7 @@ def add_words(choosen_category_id):
         Level = request.form.get('Level')
         Definition = request.form.get('Definition')
         Date = datetime.now()
-        choosen_category_id = choosen_category_id
+        Category_id = request.form.get('Categories')
         user_id = session.get('user_id')
 
         print(user_id)
@@ -204,14 +250,14 @@ def add_words(choosen_category_id):
             redirect('/signup?error=Passwords+dont+match')
 
         cur = con.cursor()
-        cur.execute(query,(Maori_word,English_word,choosen_category_id,Definition,Level,Date,user_id))
+        cur.execute(query,(Maori_word,English_word,Category_id,Definition,Level,Date,user_id))
         con.commit()
         con.close()
     error = request.args.get('error')
 
     if error == None:
         error = ""
-    return render_template("add_words.html",error=error,logged_in=is_logged_in(),choosen_category_id=choosen_category_id,categories=categories())
+    return render_template("add_words.html",error=error,logged_in=is_logged_in(),categories=categories(),is_admin=is_admin())
 
 
 if __name__ == '__main__':
